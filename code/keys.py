c8835da0deb840250867506a2d2ea8a029617ecd
Petr Krysl <pkrysl@ucsd.edu>
2021-11-19 08:57:21 -0800

    The sit was getting confused with six

--- code/keys.py
+++ code/keys.py
@@ -5,7 +5,7 @@ import sys
 
 # Petr Krysl, changed word for n
 # abcdefghimnopqrstuvwxz 
-default_alphabet = "air bat cap drum each fox gust harp is jury keel look made nose oats pin quench red sit trap urge vest whale plex yank zip".split(
+default_alphabet = "air bat cap drum each fox gust harp is jury keel look made nose oats pin quench red soot trap urge vest whale plex yank zip".split(
     " "
 )
 
